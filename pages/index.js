0d04921f3df77888e06bdab871ff148964bb53fc
Talha Zubair MaYo <talha.zubair@ammag.tech>
2021-12-18 21:35:54 +0500

    Backend Support Implemented Repo link : https://github.com/Talha-Zubair-Mayo/next-ecommerace-backend

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ import {
   CardMedia,
   CardContent,
 } from "@material-ui/core";
-const Home = () => (
+const Home = ({ ProductList }) => (
   <>
     <Layout>
       <Grid container spacing={3}>
-        {AllProducts.map((product, index) => (
+        {ProductList.map((product, index) => (
           <Grid key={index} item md={4}>
             <Product productDetails={product} />
           </Grid>
@@ -25,4 +25,16 @@ const Home = () => (
   </>
 );
 
+// Home.getInitialProps = async (ctx) => {
+//   const res = await fetch("http://localhost:8080/api/products");
+//   const json = await res.json();
+//   return { ProductList: json };
+// };
+export async function getServerSideProps({ query }) {
+  const res = await fetch(`http://localhost:8080/api/products`);
+  const json = await res.json();
+  return {
+    props: { ProductList: json }, // will be passed to the page component as props
+  };
+}
 export default Home;
